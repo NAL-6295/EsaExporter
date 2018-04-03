513f83eb635e8f982de9c199d70bfa80bb576679
hiroaki Ohhashi <naruhiron@gmail.com>
2018-04-03 23:55:08 +0900

    markdownで保存してパスも変換されるようにしてみた

--- EsaExportor/Program.cs
+++ EsaExportor/Program.cs
@@ -15,12 +15,21 @@ namespace EsaExportor
 
         private static string fromTeam;
         private static string filePath;
+
+        public enum ExportOption
+        {
+            Json = 0,
+            Markdown
+        }
+
+
         static void Main(string[] args)
         {
-            if (args.Length != 2) return;
+            if (args.Length != 3) return;
 
             var token = args[0];
             fromTeam = args[1];
+            var option = Enum.Parse<ExportOption>(args[2]);
             filePath = $@"d:\{fromTeam}.esa.io";
 
             if (!System.IO.Directory.Exists(filePath))
@@ -45,7 +54,7 @@ namespace EsaExportor
                     var jsons = JsonSerializer.Deserialize<Posts>(result);
                     foreach (var post in jsons.posts)
                     {
-                        ToLocal(post);
+                        ToLocal(post, option);
                     }
                     Console.WriteLine($"page:{page}");
                     page++;
@@ -62,7 +71,7 @@ namespace EsaExportor
 
         }
 
-        private static void ToLocal(Post post)
+        private static void ToLocal(Post post, ExportOption option)
         {
             string text = "";
             try
@@ -80,38 +89,75 @@ namespace EsaExportor
 
             var images = html.DocumentNode
                 .SelectNodes(@"//img");
-            var path = $@"{filePath}\{post.number}";
+
+            var tite = new String(post.full_name.Replace("/","_").Where(x => !System.IO.Path.GetInvalidFileNameChars().Contains(x)).ToArray());
+            var path = option == ExportOption.Json ? $@"{filePath}\{post.number}" :
+                $@"{filePath}\{tite}";
 
 
             if (images != null)
             {
-                if (!System.IO.Directory.Exists(path))
-                {
-                    System.IO.Directory.CreateDirectory(path);
-                }
-
-                using (var imageRequest = new System.Net.WebClient())
-                {
-                    foreach (var address in images
-                        .Select(x => x.Attributes["src"]))
-                    {
-                        try
-                        {
-                            imageRequest.DownloadFile(address.Value, $@"{path}\{System.IO.Path.GetFileName(address.Value)}");
-                        }
-                        catch (Exception)
-                        {
-                            //握りつぶす
-                        }
-                    }
-                }
+                DownloadImages(images, path);
 
             }
 
-            var jsonData = JsonSerializer.Serialize(post);
+            if(option == ExportOption.Json)
+            {
+                var jsonData = JsonSerializer.Serialize(post);
 
-            System.IO.File.WriteAllBytes($@"{filePath}\{post.number}.json", jsonData);
+                System.IO.File.WriteAllBytes($@"{path}.json", jsonData);
+            }
+            else
+            {
 
+                var md = post.body_md;
+                if (images != null)
+                {
+                    var files = System.IO.Directory.GetFiles(path)
+                        .Select(x => System.IO.Path.GetFileName(x))
+                        .ToArray();
+
+                    foreach (var item in images)
+                    {
+                        var url = item.Attributes["src"].Value;
+                        var fileName = files.FirstOrDefault(x => item.Attributes["src"].Value.Contains(x));
+                        if(!string.IsNullOrEmpty(fileName))
+                        {
+                            md = md.Replace(url, $@"./{tite}/{fileName}");
+                        }
+                    }
+
+                }
+
+                System.IO.File.WriteAllText($@"{path}.md", md);
+
+            }
+
+
+        }
+
+        private static void DownloadImages(HtmlAgilityPack.HtmlNodeCollection images, string path)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+
+            using (var imageRequest = new System.Net.WebClient())
+            {
+                foreach (var address in images
+                    .Select(x => x.Attributes["src"]))
+                {
+                    try
+                    {
+                        imageRequest.DownloadFile(address.Value, $@"{path}\{System.IO.Path.GetFileName(address.Value)}");
+                    }
+                    catch (Exception)
+                    {
+                        //握りつぶす
+                    }
+                }
+            }
         }
     }
 }
